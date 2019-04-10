3aa5341e9655a50e7b2b1cc8815bb89d4a9f2582
Dai Nguyen <ngocdai94@gmail.com>
2019-04-10 15:32:24 -0700

    Week 2 - Modular get/getAll/add/delete

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-// Code to launch a Node.js web server that can respond to requests
+/** 
+ * Code to launch a Node.js web server that can respond to requests
+ * */ 
 
+// Importing necessary pakages and js files for the program
+const bookCollection = require ("./lib/bookCollection");
+const querystring = require('querystring');
 const http = require("http");   // require http pakage
 const fs = require("fs");       // require file system package
 
+// Constant Variables
+const NOT_FOUND = -1;
+const WHITE_SPACE_CASE_1 = '+';
+const WHITE_SPACE_CASE_2 = '%20';
+
+// Global Variables
+var itemKey = '';
+var itemValue = '';
+
 // Initialize Node.js server and Listen on port 3000
 http.createServer((req,res) => {
-  // Get Request Path
-  const path = req.url.toLowerCase();
-  console.log(path);
+  // Get Request Path and Request Method
+  const path = req.url; //.toLowerCase();
+  var requestMethod = '';
 
-  // Response according to requested path and output data to browser
-  switch(path) {
+  if (path.indexOf('?') != NOT_FOUND)
+    requestMethod = path.substr(0, path.indexOf('?'));
+  else {
+    requestMethod = path;
+  }
+  
+  /**  
+   * Uncomment the lines below to check for URL path
+  */
+  // console.log(path);
+  // console.log('Method request is... ' + requestMethod);
+
+  // Response according to requested path/method and output data to browser
+  switch(requestMethod) {
     case '/':
     /**
      * fs.readFile will read relative file path and send a result back to the
      * call back function (annonymous function).
      * 
-     * In this annonymous function, err (error) and data will be handled.
+     * In this annonymous function, err (error) and data will be returned from the
+     * call back function.
      * Then, display data to browser if no error.
      */
       fs.readFile("public/home.html", (err, data) => {
@@ -33,6 +60,70 @@ http.createServer((req,res) => {
         res.end(data.toString());
       });
     break;
+    
+    case '/get':
+      // Return 1st index of the book collection if no search key is defined
+      // Otherwise, return the search result for the search key
+      if (path.indexOf('?') == NOT_FOUND) {
+        var data = bookCollection.getAll();
+        res.writeHead(200, {'Content-Type': 'text/JSON'});
+        res.end(JSON.stringify(data[0]));
+      } else {
+        // parsing item key and value
+        parseURL(path);
+
+        // search and return search result
+        var data = bookCollection.get(itemKey, itemValue);
+        if (data == NOT_FOUND) data = 'Not Found';
+        res.writeHead(200, {'Content-Type': 'text/JSON'});
+        res.end('Searching for ' + itemValue + ':\n' + JSON.stringify(data));
+      }
+    break;
+
+    case '/getAll':
+      var data = bookCollection.getAll();
+      res.writeHead(200, {'Content-Type': 'text/JSON'});
+      res.end(JSON.stringify(data));
+    break;
+
+    case '/add':
+      if (path.indexOf('?') == NOT_FOUND) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Nothing to add');
+      } else {
+        // parsing add request URL key and value, then add to JSON book collection
+        var itemURL = path.substr(path.indexOf('?')+1);
+        var jsonObject = querystring.parse(itemURL);
+        var result = bookCollection.add(jsonObject);
+        
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(JSON.stringify(jsonObject) + ' is added.\n' + 'Total items is ' + result.total);
+      }
+    break;
+
+    case '/delete':
+      // Delete a requested item if in the list
+      if (path.indexOf('?') == NOT_FOUND) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Please specify an item to delete!');
+      } else {
+        // parsing item key and value
+        parseURL(path);
+
+        // delete and return result
+        var data = bookCollection.delete(itemKey, itemValue);
+        if (data != NOT_FOUND) {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(itemValue + ' removed.');
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(itemValue + ' not removed or have been removed.');
+        }
+
+        // check data in terminal
+        // console.log('\n\n' + data + '\n\n');
+      }
+    break;
 
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
@@ -41,41 +132,13 @@ http.createServer((req,res) => {
   }
 }).listen(process.env.PORT || 3000);
 
+function parseURL (path) {
+  // parsing item key and value
+  let itemURL = path.substr(path.indexOf('?')+1);
+  itemKey = itemURL.split('=')[0];
+  itemValue = itemURL.split('=')[1];
 
-
-/***** More Advanced Code from the instructor, Brenden West. ******/
-
-// const http = require("http")
-// const fs = require("fs"); 
-
-// function serveStatic(res, path, contentType, responseCode){
-//   if(!responseCode) responseCode = 200;
-//   console.log(__dirname + path)
-//   fs.readFile(__dirname + path, function(err, data){
-//       if(err){
-//         res.writeHead(500, {'Content-Type': 'text/plain'});
-//         res.end('Internal Server Error');
-//       }
-//       else{
-//         res.writeHead(responseCode, {'Content-Type': contentType});
-//         res.end(data);
-//       }
-//   });
-// }
-
-// http.createServer((req,res) => {
-//   var path = req.url.toLowerCase();
-//   switch(path) {
-//     case '/': 
-//       serveStatic(res, '/public/home.html', 'text/html');
-//       break;
-//     case '/about':
-//       //res.writeHead(200, {'Content-Type': 'text/plain'});
-//       // res.end('About');
-//       serveStatic(res, '/about', 'text/plain');
-//       break;
-//     default:
-//       res.writeHead(404, {'Content-Type': 'text/plain'});
-//       res.end('404:Page Not Found.');
-//   }
-// }).listen(process.env.PORT || 3000);
+  // replace white spaces for two words names
+  if (itemValue.indexOf(WHITE_SPACE_CASE_1) != NOT_FOUND) itemValue = itemValue.replace('+', ' ');
+  if (itemValue.indexOf(WHITE_SPACE_CASE_2) != NOT_FOUND) itemValue = itemValue.replace('%20', ' ');
+}
